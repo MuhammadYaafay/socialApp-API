7c7d09753a1cad0380eba59212d2192bf3451467
yaafay <muhammad.yaffay@gmail.com>
2024-12-21 21:23:22 +0500

    inAdd new files and configurations for various modules and dependencies

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+
+dotenv.config();
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message);
+        process.exit(1); 
+    }
+};
+
+connectDB();
+
+
+app.listen(8800, () => {
+    console.log("Backend server is running on port 8800");
+});
