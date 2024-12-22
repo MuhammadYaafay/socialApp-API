b7214b621934bde180605f899163c3f10ffd8f56
yaafay <muhammad.yaffay@gmail.com>
2024-12-22 20:15:37 +0500

    Add post model and routes for creating, updating, deleting, and liking posts

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
+const postRoute = require("./routes/post")
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 const connectDB = async () => {
     try {
